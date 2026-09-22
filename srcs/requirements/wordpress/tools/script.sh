29a3f0e5960485f61b17e2ed896bc88ff0bf9c39
jmondela <jmondela@jmondela.jmondela>
2026-09-22 19:43:33 +0200

    sy last corrections

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -17,7 +17,7 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
 						--dbhost=mariadb \
 						--allow-root
 
-	wp core install		--url=${USERID}.42.fr \
+	wp core install		--url=https://${USERNAME}.42.fr \
 						--title=${WP_TITLE} \
 						--admin_user=${WP_ADMIN} \
 						--admin_password=${WP_ADMIN_PASSWORD} \
